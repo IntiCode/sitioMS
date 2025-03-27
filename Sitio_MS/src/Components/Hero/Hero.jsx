41802b9bf5eff678c44975f763fa617c57273d67
Vane-SDeV <vanesa.soria.p@gmail.com>
2025-03-27 16:01:04 -0300

    hero

--- Sitio_MS/src/Components/Hero/Hero.jsx
+++ Sitio_MS/src/Components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className='Hero_container'>
                 
                 <div className='Hero_logo'>
-                    <img className='heroImg' src={logoHero} alt="logo" width="200" />
+                    <img className='heroImg' src={logoHero} alt="logo" width="250" />
                 </div>
                 
                 <div className="Hero_buttons d-flex gap-2 mb-2">
@@ -26,6 +26,20 @@ const Hero = () => {
                     </div>
 
                 </div>
+            </div>
+            
+            {/* Onda separadora */}
+            <div className="Hero_wave">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1440 320"
+                >
+                    <path
+                        fill="#ffffff"
+                        fillOpacity="1"
+                        d="M0,64L40,58.7C80,53,160,43,240,80C320,117,400,203,480,213.3C560,224,640,160,720,149.3C800,139,880,181,960,197.3C1040,213,1120,203,1200,176C1280,149,1360,107,1400,85.3L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+                    ></path>
+                </svg>
             </div>
 
 
