15099057a4f53fee8efa1d9da45ad80f715495f0
Estefania Vago <ecvago@hotmail.com>
2025-03-27 12:34:08 -0300

    contacto y footer estilos

--- Sitio_MS/src/Components/Footer/Footer.jsx
+++ Sitio_MS/src/Components/Footer/Footer.jsx
@@ -2,28 +2,29 @@ import React from 'react'
 import './Footer.css';
 import imagen from '../../assets/hero/logopeque.png';
 import { FaInstagram, FaLinkedin, FaFacebookSquare } from "react-icons/fa";
+import { Link } from 'react-scroll';
 
 const Footer = () => {
     return (
-        <section id="footer" className="section">
+        <section id="footer" className="footer">
 
             <div className='footer-container'>
                 <div className='footer-logo'>
                     <div>
                         <img src={imagen} alt="Logo"  width='80px'/>
-                    </div>
-                    <div>
                         <p>Consultora HSE</p>
                     </div>
-
+                   
                 </div>
 
 
-                <div className="footer-secciones ">
-                    <h3>Home</h3>
-                    <h3>About us</h3>
-                    <h3>Servicios</h3>
-                    <h3>Contacto</h3>
+                <div className="footer-secciones">
+                <p><Link to="inicio"   smooth={true} duration={200}>Inicio</Link></p>
+                <p><Link to="empresa"   smooth={true} duration={500}>Empresa</Link></p>
+                <p><Link to="servicios"   smooth={true} duration={200}>Servicios</Link></p>
+                <p><Link to="contacto"   smooth={true} duration={500}>Contacto</Link></p>
+                
+                
                 </div>
 
                 <div className='footer-redes'>
