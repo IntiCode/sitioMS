6416c476485fbcf8d0538a3b08bc9b2645c34521
Estefania Vago <ecvago@hotmail.com>
2025-03-28 12:31:10 -0300

    cliente

--- Sitio_MS/src/Components/Clientes/Carousel.jsx
+++ Sitio_MS/src/Components/Clientes/Carousel.jsx
@@ -1,6 +1,10 @@
 import Carousel from 'react-bootstrap/Carousel';
 import './Carousel.css';
 import img1 from '../../assets/clientes/Belona.png';
+import img2 from '../../assets/clientes/tecpetrol.png';
+import img3 from '../../assets/clientes/sodexo.jpg';
+import img4 from '../../assets/clientes/cunmalleu.png';
+import img5 from '../../assets/clientes/polyar.png';
 
 
 
@@ -10,14 +14,14 @@ function Carrousel() {
             <Carousel.Item>
                 <div className="d-flex justify-content-around"> 
                     <img id='img' className="d-block" src={img1} alt="Logo 1" />
-                    <img id='img' className="d-block" src={img1} alt="Logo 2" />
-                    <img id='img' className="d-block" src={img1} alt="Logo 3" />
-                    <img id='img' className="d-block" src={img1} alt="Logo 3" />
+                    <img id='img' className="d-block" src={img2} alt="Logo 2" />
+                    <img id='img' className="d-block" src={img3} alt="Logo 3" />
+                    <img id='img' className="d-block" src={img4} alt="Logo 3" />
                 </div>
             </Carousel.Item>
             <Carousel.Item>
                 <div className="d-flex justify-content-around">
-                    <img id='img' className="d-block" src={img1} alt="Logo 1" />
+                    <img id='img' className="d-block" src={img5} alt="Logo 1" />
                     <img id='img' className="d-block" src={img1} alt="Logo 2" />
                     <img id='img' className="d-block" src={img1} alt="Logo 3" />
                     <img id='img' className="d-block" src={img1} alt="Logo 3" />
